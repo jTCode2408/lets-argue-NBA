b7398fe563893d620a1fce7ff9734ad512710c5e
Jasmine Terry <jasmine-terry91@gmail.com>
2020-08-06 18:00:30 -0400

    api call coming back with hardcoded id, basic input form made, nosubmit yet

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,3 +2,60 @@
 //add to Home page, will be main basic app functionality
 // api/v1/season_averages?season=2018&player_ids[]=1&player_ids[]=2 will return regular season averages for player_ids 1 and 2. ONLY NEED 1 PLAYER HERE
 //ALLOW SEASON DROPDOWN
+import React, {useState, useEffect, useParams} from 'react';
+import axios from 'axios';
+
+const Player=()=>{
+    const [player, setPlayer]=useState('');
+    const [year, setYear]= useState(2020);
+    
+    
+
+   const handleSubmit=(e)=>{
+        e.preventDefault();
+        setPlayer(player)
+    }
+
+    const handleChange = (e)=>{
+        const splitting = e.target.value.split(" ").join("_");
+        if (splitting.length > 0){
+            setPlayer({player: splitting})
+
+        } else{
+            alert(' Please enter player name');
+        }
+
+    }
+    
+    const getPlayer= useEffect((player)=>{
+        axios.get('https://www.balldontlie.io/api/v1/season_averages?season=2006&player_ids[]=237')
+        .then(res=>{
+            console.log(res.data.data)
+            setPlayer(res.data.data[0])
+           
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+        
+     }, [])
+    
+
+
+
+    return(
+        <div className="playerCont">
+            Search a plyer's stats
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input type="text"
+                    
+                    onChange={handleChange}
+                    />
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Player;
--- src/components/Versus.js
+++ src/components/Versus.js
@@ -1,3 +1,14 @@
 //for searching 2 players...results returned
 // api/v1/season_averages?season=2018&player_ids[]=1&player_ids[]=2 will return regular season averages for player_ids 1 and 2.
 //ALL SEASON DROPDOWN
+import React, {useState} from 'react';
+import axios from 'axios';
+
+const Versus =()=>{
+    return(
+        <div>
+            Arguing Time!
+        </div>
+    )
+
+}
