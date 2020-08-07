7bd26fee8d4696f878a8a60554c4ffe52e72af69
Jasmine Terry <jasmine-terry91@gmail.com>
2020-08-07 01:49:10 -0400

    still working

--- src/components/Player.js
+++ src/components/Player.js
@@ -7,21 +7,22 @@ import React, {useState, useEffect, useParams} from 'react';
 import axios from 'axios';
 
 const Player=()=>{
-    const [player, setPlayerId]=useState('');
+    const [player, setPlayer]=useState('');
+    const [stats, setStats]=useState({});
     const [year, setYear]= useState(2000);
-    
-    
+
 
    const handleSubmit=(e)=>{
         e.preventDefault();
-        getPlayerId();
+        getPlayer();
+        console.log('SUBMITTING', player);
         
     }
 
     const handleChange = (e)=>{
         const splitting = e.target.value.split(" ").join("_");
         if (splitting.length > 0){
-            setPlayerId({ player: splitting})
+            setPlayer(splitting)
 
         } else{
             alert(' Please enter player name');
@@ -29,25 +30,38 @@ const Player=()=>{
 
     }
     
-   const getPlayerId=()=>{
+   useEffect(()=>{
     axios.get(`https://www.balldontlie.io/api/v1/players?search=${player}`)
-    .then(res=>{
-        console.log(res.data.data)
-         //await getPlayer(res.data.data[0].id)
+    .then(  res=>{
+        console.log('FROM ID CALL', res.data.data[0].id)
+        setPlayer(res.data.data[0].id)
+         
     })
     .catch(err=>{
         console.log(err)
     })
+}, [player])
 
-   }
-
-
-    const getPlayer=(playerId)=> {
-        
-        axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2002&player_ids[]=${playerId}`)
-        .then(res=>{
+    const getPlayer=()=>{
+        axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2002&player_ids[]=${player}`)
+        .then(async res=>{
             console.log(res.data.data[0])
-            setPlayerId(res.data.data[0])
+            setStats(res.data.data[0])
+           
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+    }
+   
+
+
+    const getPlayerData=(PID)=> {
+        
+        axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2002&player_ids[]=${PID}`)
+        .then(async res=>{
+            console.log(res.data.data[0])
+            setStats(res.data.data[0])
            
         })
         .catch(err =>{
@@ -64,10 +78,10 @@ const Player=()=>{
             Search a plyer's stats
             <div>
                 <form onSubmit={handleSubmit}>
-                    <label> Name
+                    <label> Player:
                     <input type="text"
-                  
                     onChange={handleChange}
+                
                     />
                     </label>
                     <input type="submit" value="Submit"/>
